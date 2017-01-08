52b1db027d90194c883ceec1c6fc804d2ffd6bbe
cqhaibin <kxhaibin@163.com>
2017-01-08 22:02:18 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app"> 
+    <tag width="300px"></tag>
+  </div>
+</template>
+
+<script>
+import Tag from './components/Tag'
+
+export default {
+  name: 'app',
+  components: {
+    Tag
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
