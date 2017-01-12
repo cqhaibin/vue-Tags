439ce1776979afcf0d1ecb6e1b64d26fcb83bac8
cqhaibin <kxhaibin@163.com>
2017-01-12 20:42:27 +0800

    更新包
    Signed-off-by: cqhaibin <kxhaibin@163.com>

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="app"> 
+  <div id="app">  
     <tag width="300px"></tag>
   </div>
 </template>
--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
+import '../node_modules/bootstrap/dist/css/bootstrap.css'
 import Vue from 'vue'
 import App from './App'
 
