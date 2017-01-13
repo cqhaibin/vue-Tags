ce16255848d4fbe0669b83aca5d95814a4cb4058
cqhaibin <kxhaibin@163.com>
2017-01-13 08:34:42 +0800

    1.加入element-ui的引入。
    Signed-off-by: cqhaibin <kxhaibin@163.com>

--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 import '../node_modules/bootstrap/dist/css/bootstrap.css'
 import Vue from 'vue'
+import ElemUi from 'element-ui'
+import 'element-ui/lib/theme-default/index.css'
 import App from './App'
+
+Vue.use(ElemUi)
 
 /* eslint-disable no-new */
 new Vue({
